826d8d9f7cfc236f9ccc1a5bb35a5dfb1f06225f
caiguoqing <caiguoqing@addnewer.com>
2017-03-03 18:37:38 +0800

    637

--- src/app/app.Controller.js
+++ src/app/app.Controller.js
@@ -4,7 +4,27 @@
 'use strict';
 
 export default class appCtrl{
-    constructor(){
+    constructor($http,$scope,$state){
         'ngInject';
+        Object.assign(this,{$http,$scope,$state})
+
+        this.menu = [
+            {
+                id: 1,
+                name:'HomePage',
+                href: 'homePageList'
+            },
+            {
+                id: 2,
+                name:'Campaign',
+                href: 'Campaign'
+            }
+        ];
+        this.menuClick = function(index){
+            if($state.includes(this.menu[index]['href'])){
+                return true;
+            }
+            return false;
+        }
     }
 }
